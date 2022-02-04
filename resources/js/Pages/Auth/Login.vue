c5f44bfbc071f579ec6e8d3d40a5a0cab5d755d9
Alexander Horner <mail@alexanderhorner.com>
2022-02-04 09:00:39 +0100

    Misc

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -14,33 +14,33 @@
 
         <form @submit.prevent="submit">
             <div>
-                <jet-label for="email" value="Email" />
+                <jet-label for="email" value="E-Mail" />
                 <jet-input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus />
             </div>
 
             <div class="mt-4">
-                <jet-label for="password" value="Password" />
+                <jet-label for="password" value="Passwort" />
                 <jet-input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
                 <label class="flex items-center">
                     <jet-checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                    <span class="ml-2 text-sm text-gray-600">Angemeldet bleiben</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <Link v-if="canResetPassword" :href="route('password.request')" class="mr-3 underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
+                    Passwort vergessen?
                 </Link>
 
                 <Link :href="route('register')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Register
+                    Registrieren
                 </Link>
 
                 <jet-button class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
+                    Anmelden
                 </jet-button>
             </div>
         </form>
