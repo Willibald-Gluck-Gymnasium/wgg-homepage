7dcd707f301655e8e291147c3e8c68e573a06903
Erdragh <enderjanyt@gmail.com>
2022-02-18 15:12:15 +0100

    ported results and collections to inertia/vue

---
+++ resources/js/Pages/Collection.vue
@@ -0,0 +1,44 @@
+<template>
+    <Head title="Collection" />
+
+    <main-header></main-header>
+    <secondary-header></secondary-header>
+    <flickity-slide></flickity-slide>
+    <div class="contentwrapper">
+        <collection :category="category"></collection>
+        <card-cluster style="margin-top: 15px"></card-cluster>
+    </div>
+    <vue-footer></vue-footer>
+</template>
+
+<style>
+    
+</style>
+
+<script>
+    import { Link, Head } from '@inertiajs/inertia-vue3';
+
+    import MainHeader from '@components/MainHeader'
+    import SecondaryHeader from '@components/SecondaryHeader'
+    import Collection from '@components/Collection'
+    import CardCluster from '@components/CardCluster'
+    import VueFooter from '@components/VueFooter'
+
+    export default {
+        components: {
+            Head,
+            Link,
+            MainHeader,
+            SecondaryHeader,
+            Collection,
+            CardCluster,
+            VueFooter
+        },
+
+        props: {
+            canLogin: Boolean,
+            canRegister: Boolean,
+            category: String
+        }
+    }
+</script>
