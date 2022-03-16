2f27b96e0a68642df585c5bb2522d88292f8bd10
Erdragh <enderjanyt@gmail.com>
2022-03-16 12:32:56 +0100

    remove uneccessary Search.vue

--- resources/js/Pages/Results.vue
+++ resources/js/Pages/Results.vue
@@ -3,9 +3,31 @@
 
     <main-header></main-header>
     <secondary-header></secondary-header>
-    <flickity-slide></flickity-slide>
     <div class="contentwrapper">
-        <search></search>
+        <form class="menu-container">
+            <div class="bar search">
+                <div class="field spread">
+                    <span>Suchbegriff</span>
+                    <input name="q" type="text" :value=this.initSearchParam>
+                </div>
+                <div class="field">
+                    <span>Sortieren nach</span>
+                    <select name="s" id="sort">
+                        <option value="r">Relevanz</option>
+                        <option value="a">Alphabet</option>
+                        <option value="d">Datum</option>
+                    </select>
+                </div>
+                <div class="field">
+                    <span></span>
+                    <button id="submit" class="icon">
+                        <i class='material-icons'>search</i>
+                    </button>
+                </div>
+            </div>
+            <!-- <div class="bar">
+            </div> -->
+        </form>
         <search-results><b>TODO</b></search-results>
     </div>
     <vue-footer></vue-footer>
@@ -20,7 +42,6 @@
 
     import MainHeader from '@components/MainHeader'
     import SecondaryHeader from '@components/SecondaryHeader'
-    import Search from '@components/Search'
     import SearchResults from '@components/SearchResults'
     import SearchResult from '@components/SearchResult'
     import VueFooter from '@components/VueFooter'
@@ -31,7 +52,6 @@
             Link,
             MainHeader,
             SecondaryHeader,
-            Search,
             SearchResults,
             SearchResult,
             VueFooter
@@ -40,6 +60,42 @@
         props: {
             canLogin: Boolean,
             canRegister: Boolean
+        },
+
+        //Search Menu
+        data: () => ({
+        initSearchParam: "Search Param"
+        }),
+        methods: {
+            checkURL() {
+                const url = window.location.search;
+                const urlParams = new URLSearchParams(url);
+                this.initSearchParam = urlParams.get('q');
+            }
+        },
+        mounted() {
+            this.checkURL();
         }
     }
 </script>
+
+<style lang="scss" scoped>
+$height: 35px;
+
+.menu-container {
+    // margin-top: 25px;
+    // width: 100%;
+    padding: 15px;
+    background: var(--clr-bg-primary);
+    border-bottom-left-radius: 15px;
+    border-bottom-right-radius: 15px;
+    box-shadow: 0 0 15px rgba(0,0,0,.1);
+}
+
+@media only screen and (max-width: 700px) {
+    .menu-container {
+        margin-left: 10px !important;
+        margin-right: 10px !important;
+    }
+}
+</style>
