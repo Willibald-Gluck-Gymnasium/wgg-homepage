3348866a7fdb76f1675ce9edc6e8bf47251829ac
ScriptorGames <f.schreiber.2000@web.de>
2022-04-01 14:33:26 +0200

    Bilder Komponenten und co.
    Bilderkomponenten ausgetauscht und Artikel vervollständigt.

--- resources/js/images.js
+++ resources/js/images.js
@@ -23,6 +23,7 @@ const images = {
     schuelergenossenschaft_gruendung_slide: require("@img/schuelergenossenschaft.jpg?pipeline=slide"),
     schuelergenossenschaft_gruendung_thumbnail: require("@img/schuelergenossenschaft.jpg?pipeline=thumbnail"),
     schuelergenossenschaft_gruendung_1: require("@img/schuelergenossenschaft.jpg?pipeline=article"),
+    
     corona_virus_close_up_thumbnail: require('@img/corona-virus-close-up.jpg?pipeline=thumbnail'),
     corona_virus_close_up_slide: require('@img/corona-virus-close-up.jpg?pipeline=slide'),
 
@@ -49,18 +50,28 @@ const images = {
 
     wirtschaft_neues_aus_wr_thumbnail: require('@img/unterricht_wirtschaft_neues_aus_wr_1.png?pipeline=thumbnail'),
     wirtschaft_neues_aus_wr_slide_image: require('@img/unterricht_wirtschaft_neues_aus_wr_1.png?pipeline=slide'),
-
     wirtschaft_neues_aus_wr_1: require('@img/unterricht_wirtschaft_neues_aus_wr_1.png?pipeline=article'),
     wirtschaft_neues_aus_wr_2: require('@img/unterricht_wirtschaft_neues_aus_wr_2.png?pipeline=article'),
     wirtschaft_neues_aus_wr_3: require('@img/unterricht_wirtschaft_neues_aus_wr_3.png?pipeline=article'),
     wirtschaft_neues_aus_wr_4: require('@img/unterricht_wirtschaft_neues_aus_wr_4.png?pipeline=article'),
+    
     wirtschaft_naturkosmetikspende: require('@img/unterricht_wirtschaft_naturkosmetikspende.jpg?pipeline=article'),
 
     Dull_WR_Gründung_Schülergenossenschaft_Bild_Logo: require('@img/04Dull_WR_Gründung-Schülergenossenschaft_Bild_Logo.jpg?pipeline=article'),
     Dull_WR_Gründung_Schülergenossenschaft_Bild2: require('@img/04Dull_WR_Gründung-Schülergenossenschaft_Bild2.JPG?pipeline=article'),
     Dull_WR_Gründung_Schülergenossenschaft_Bild: require('@img/04Dull_WR_Gründung-Schülergenossenschaft_Bild.JPG?pipeline=article'),
 
-    mathe_17_wb: require('@img/mathe_17_wb.jpg?pipeline=article')
+    mathe_17_wb: require('@img/mathe_17_wb.jpg?pipeline=article'),
+
+    gemeinschaft_direktorat_00_schiffer: require('@img/gemeinschaft_direktorat_00_schiffer.jpg?pipeline=article'),
+    gemeinschaft_direktorat_00_kroiss: require('@img/gemeinschaft_direktorat_00_kroiss.jpg?pipeline=article'),
+    gemeinschaft_direktorat_00_Holler: require('@img/gemeinschaft_direktorat_00_Holler.jpg?pipeline=article'),
+    gemeinschaft_direktorat_00_sachs: require('@img/gemeinschaft_direktorat_00_sachs.jpg?pipeline=article'),
+    gemeinschaft_direktorat_00_wagenhofer: require('@img/gemeinschaft_direktorat_00_wagenhofer.jpg?pipeline=article'),
+
+    mathe_fachprofil_Kompetenzstrukturmodell: require('@img/mathe_fachprofil_Kompetenzstrukturmodell.jpg?pipeline=article'),
+    mathe_fachprofil_slide: require('@img/mathe_fachprofil_Kompetenzstrukturmodell.jpg?pipeline=slide'),
+    mathe_fachprofil_thumbnail: require('@img/mathe_fachprofil_Kompetenzstrukturmodell.jpg?pipeline=thumbnail')
 }
 
 export default images
