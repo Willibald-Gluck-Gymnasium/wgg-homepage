289f6223b3ff64298d2f16884e47561096e7f4c5
Alexander Horner <mail@alexanderhorner.com>
2022-09-27 10:29:23 +0200

    Make tag search work.

---
+++ pages/tag/[slug].vue
@@ -0,0 +1,36 @@
+<script setup>
+const tag = useRoute().params.slug
+const cards = await queryContent().where({ tags: { $contains: tag } }).only(['title', 'author', 'category', 'tags', 'thumbnail', '_path']).find()
+console.log(cards);
+</script>
+
+<template>
+    <NuxtLayout>
+        <main>
+            <template v-if="cards.length > 0">
+                <h1>Artikel verwandt mit: {{ tag }}</h1>
+                <CardCluster  :cards="cards"/>
+            </template>
+
+            
+            <template v-else>
+                <h1 style="margin-bottom: 4em;">Keine Artikel verwandt mit: {{ tag }}</h1>
+            </template>
+        </main>
+    </NuxtLayout>
+</template>
+
+
+<style lang="scss" scoped>
+h1 {
+    margin-left: auto;
+    margin-right: auto;
+    margin: 1em auto 0.2em;
+    width: calc(100% - 20px);
+    max-width: 1080px;
+
+    @media (min-width: 500px) {
+        width: calc(100% - 40px);
+    }
+}
+</style>
