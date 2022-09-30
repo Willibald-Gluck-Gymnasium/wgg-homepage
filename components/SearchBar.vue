9f881dc466c7f6839e01ce264c7d309878ab6ad6
Alexander Horner <mail@alexanderhorner.com>
2022-09-30 09:30:25 +0200

    Fix border-radius

--- components/SearchBar.vue
+++ components/SearchBar.vue
@@ -26,7 +26,7 @@ const unfocusSearchBox = () => {
 
 <template>
     <AisInstantSearch :search-client="searchClient" index-name="articles">
-        <AisSearchBox placeholder="Suche hier..." ref="searchbox" @focus="showResults = true" @blur="showResults = false" /> 
+        <AisSearchBox placeholder="Suche hier..." ref="searchbox" @focus="showResults = true" @blur="showResults = true" /> 
 
         <AisStateResults>
             <template v-slot="{ results: { hits, query } }">
@@ -75,8 +75,8 @@ const unfocusSearchBox = () => {
     </AisInstantSearch>
 </template>
 
-<style lang="scss">
-.ais-InstantSearch {
+<style lang="scss" scoped>
+.ais-InstantSearch :deep() {
     width: 100%;
     max-width: 400px;
     height: 42px;
@@ -100,7 +100,7 @@ const unfocusSearchBox = () => {
                 box-shadow: none;
                 border: none;
                 color: black;
-                background-color: hsl(0, 0%, 90%);
+                background-color: hsl(0, 0%, 95%);
                 border-radius: 21px;
                 width: 100%;
                 margin: 0;
@@ -130,10 +130,13 @@ const unfocusSearchBox = () => {
         overflow: hidden;
 
         .ais-Hits-list {
+
             .ais-Hits-item {
                 border-radius: 0;
-                background-color: hsl(0, 0%, 90%);
-                border-top: 1px solid hsl(0, 0%, 70%);
+                box-shadow: initial;
+                background-color: hsl(0, 0%, 95%);
+                border: none;
+                border-top: 1px solid hsl(0, 0%, 80%);
                 padding: 0;
 
                 &:hover {
