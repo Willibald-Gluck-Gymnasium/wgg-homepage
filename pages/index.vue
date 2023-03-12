a8a6afb57b5c2441f7abd3b88060a1205e86ebc3
Alexander Horner <mail@alexanderhorner.com>
2023-03-12 16:51:36 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -17,39 +17,39 @@ const attributesToFetch = ['_path', 'date', 'title', 'tags', 'thumbnail', 'pinne
 
 const highlightedArticlesCards = await queryContent('/').sort({ title: 1, date: -1, }).where({ pinned: { $eq: true }, hidden: { $ne: true } }).only(attributesToFetch).find()
 
-const now = new Date(Date.now()).toISOString()
+// const now = new Date(Date.now()).toISOString()
 
-const queryForUnpinnedArticles = { 
-    pinned: { $ne: true },
-    hidden: { $ne: true },
-    $or: [
-        { expireOn: { $exists: false } },
-        { expireOn: { $gt: now } }
-    ]
-}
+// const queryForUnpinnedArticles = { 
+//     pinned: { $ne: true },
+//     hidden: { $ne: true },
+//     $or: [
+//         { expireOn: { $exists: false } },
+//         { expireOn: { $gt: now } }
+//     ]
+// }
 
-const articleCards = ref(await queryContent('/').sort({ title: 1, date: -1, }).where(queryForUnpinnedArticles).only(attributesToFetch).limit(20).find())
+// const articleCards = ref(await queryContent('/').sort({ title: 1, date: -1, }).where(queryForUnpinnedArticles).only(attributesToFetch).limit(20).find())
 
 
 
-const loadMoreArticlesButtonDisabled = ref(false)
-const allArticlesLoaded = ref(false)
-const getRestOfArticles = async () => {
-    loadMoreArticlesButtonDisabled.value = true
+// const loadMoreArticlesButtonDisabled = ref(false)
+// const allArticlesLoaded = ref(false)
+// const getRestOfArticles = async () => {
+//     loadMoreArticlesButtonDisabled.value = true
 
-    const { data, pending, error, refresh } = await useAsyncData(
-        'fetchRemainingArticles', 
-        () => queryContent('/').sort({ title: 1, date: -1, }).where(queryForUnpinnedArticles).only(attributesToFetch).skip(20).find()
-    )
+//     const { data, pending, error, refresh } = await useAsyncData(
+//         'fetchRemainingArticles', 
+//         () => queryContent('/').sort({ title: 1, date: -1, }).where(queryForUnpinnedArticles).only(attributesToFetch).skip(20).find()
+//     )
 
-    if (error.value === null) {
-        articleCards.value.push(...data.value)
-        allArticlesLoaded.value = true
-    } else {
-        loadMoreArticlesButtonDisabled.value = false
-        console.error("Error Loading more articles")   
-    }
-}
+//     if (error.value === null) {
+//         articleCards.value.push(...data.value)
+//         allArticlesLoaded.value = true
+//     } else {
+//         loadMoreArticlesButtonDisabled.value = false
+//         console.error("Error Loading more articles")   
+//     }
+// }
 
 useHead({
     title: '',
@@ -83,7 +83,7 @@ useHead({
             </ClientOnly>
         </template>
        
-
+        <!--
         <h1>Neuigkeiten</h1>
         
         <ClientOnly>
@@ -95,6 +95,8 @@ useHead({
         </ClientOnly>
 
         <Button style="margin: 1rem auto; border-radius: 15px;" v-if="!allArticlesLoaded" :loading="loadMoreArticlesButtonDisabled" @click="getRestOfArticles()">Alle Artikel laden</Button>
+        -->
+
 
     </NuxtLayout>
 </template>
