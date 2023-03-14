83af52a0fe1a8338454317ea15733b6cebf10481
Alexander Horner <mail@alexanderhorner.com>
2023-03-14 22:32:21 +0100

    make search slug optional

--- pages/search/[[slug]].vue
+++ pages/search/[[slug]].vue
@@ -1,22 +1,10 @@
 <script setup>
 const searchquery = ref(useRoute().params.slug)
 
-// const searchAPICall = useLazyAsyncData(
-//     'searchbig',
-//     () => $fetch('/api/search', {
-//         params: {
-//             query: searchquery.value
-//         },
-//     }),
-//     { 
-//         server: false,
-//         watch: searchquery
-//     }
-// )
-
 const searchAPICall = useLazyFetch('/api/search', {
     params: {
-        query: searchquery
+        query: searchquery,
+        limit: 100
     },
     server: false,
     watch: [searchquery],
