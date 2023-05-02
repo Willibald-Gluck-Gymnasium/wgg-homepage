69e84888739c818125976e1492bcdcba73478904
IliHanSoLow <ilian-ode@gmx.de>
2023-05-02 14:39:28 +0200

    button orange and documentation

--- components/content/Gallery.vue
+++ components/content/Gallery.vue
@@ -89,12 +89,7 @@ export default {
 <style lang="scss">
 /* CSS */
 .button-36 {
-  background-image: linear-gradient(
-    92.88deg,
-    #455eb5 9.16%,
-    #5643cc 43.89%,
-    #673fd7 64.72%
-  );
+  background-color: #ff5e0e;
   border-radius: 8px;
   border-style: none;
   box-sizing: border-box;
@@ -104,9 +99,11 @@ export default {
   font-family: "Inter UI", "SF Pro Display", -apple-system, BlinkMacSystemFont,
     "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
     sans-serif;
-  font-size: 16px;
+  font-size: 50px;
   font-weight: 500;
-  height: 4rem;
+  justify-content: center;
+  align-items: center;
+  height: 3, 75rem;
   padding: 0 1.6rem;
   text-align: center;
   text-shadow: rgba(0, 0, 0, 0.25) 0 3px 8px;
@@ -114,10 +111,13 @@ export default {
   user-select: none;
   -webkit-user-select: none;
   touch-action: manipulation;
+  margin-left: 5px;
+  margin-right: 5px;
+  opacity: 0.8;
 }
 
 .button-36:hover {
-  box-shadow: rgba(80, 63, 205, 0.5) 0 1px 30px;
+  box-shadow: rgba(255, 81, 0, 0.5) 0 1px 30px;
   transition-duration: 0.1s;
 }
 
